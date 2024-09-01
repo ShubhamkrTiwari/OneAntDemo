071c09dce5396b0b58917b99d23e33b645b3fedc
ShubhamkrTiwari <skt916606@gmail.com>
2024-09-01 15:53:53 +0530

    new

--- app/src/main/java/com/example/oneantdemo/Homepage.kt
+++ app/src/main/java/com/example/oneantdemo/Homepage.kt
@@ -1,6 +1,9 @@
 package com.example.oneantdemo
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +14,10 @@ class Homepage : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_homepage)
-
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this,Onboarding::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
     }
 }
