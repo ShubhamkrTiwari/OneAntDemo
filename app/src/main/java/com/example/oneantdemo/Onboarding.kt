0080acea33a0c2e4f3f59e41e2ae785d78db85aa
ShubhamkrTiwari <skt916606@gmail.com>
2024-09-25 13:36:22 +0530

    new

--- app/src/main/java/com/example/oneantdemo/Onboarding.kt
+++ app/src/main/java/com/example/oneantdemo/Onboarding.kt
@@ -1,17 +1,25 @@
 package com.example.oneantdemo
 
-import android.os.Build.VERSION_CODES.R
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class Onboarding : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(com.example.oneantdemo.R.layout.activity_homepage)
+        setContentView(R.layout.activity_homepage)
 
+        val getStartButton = findViewById<Button>(R.id.getstart)
+
+        getStartButton.setOnClickListener {
+            val intent = Intent(this, Servicescontent::class.java)
+            startActivity(intent)
+        }
     }
+
 }
