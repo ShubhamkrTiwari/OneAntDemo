3e81371a1a0356bb88f339a38dc017ef351135ed
ShubhamkrTiwari <skt916606@gmail.com>
2024-10-24 20:05:09 +0530

    new

--- app/src/main/java/com/example/oneantdemo/Servicescontent.kt
+++ app/src/main/java/com/example/oneantdemo/Servicescontent.kt
@@ -1,6 +1,8 @@
 package com.example.oneantdemo
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,12 @@ class Servicescontent : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_servicescontent)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val reachout = findViewById<Button>(R.id.reachout)
+
+        reachout.setOnClickListener {
+            val intent = Intent(this, ContactInfo::class.java)
+            startActivity(intent)
         }
     }
 }
